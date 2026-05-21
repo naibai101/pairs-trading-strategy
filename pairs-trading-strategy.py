b63bb781358063faba62a277cdfb89729030140b
Naisha Sarna <naisha.sarna@gmail.com>
2026-05-21 16:42:31 -0500

    adding the working file in main

---
+++ pairs-trading-strategy.py
@@ -0,0 +1,13 @@
+#
+# pairs trading strategy for markets
+#
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+import statsmodels as sm
+import matplotlib as plt
+import yfinance as yf
+
+CORRELATION_NUMBER = 0.8
+
